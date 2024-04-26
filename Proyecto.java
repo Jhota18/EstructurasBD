b00d20ad95119441625a6493f1799495d5bb5cfa
Sala K 404 <jhonatangomez_18@hotmail.com>
2024-04-25 19:46:08 -0500

    Tercer push

--- Proyecto.java
+++ Proyecto.java
@@ -7,7 +7,7 @@ public class Proyecto {
         LinkedList<Empleado> empleados= new LinkedList();
         Scanner sc= new Scanner(System.in);
         for (int i = 0; i < 3; i++) {
-            Empleado item = new Empleado(null, null, null, i, null)
+            Empleado item = new Empleado(null, null, null, i, null);
             System.out.println("Ingrese el nombre:");
             item.setNombre(sc.next());
             System.out.println("Ingrese el apellido:");
