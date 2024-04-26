aeeae179701b61f7712b6905a75f388acb291d6e
Sala K 404 <jhonatangomez_18@hotmail.com>
2024-04-25 19:44:51 -0500

    Segundo push

--- Proyecto.java
+++ Proyecto.java
@@ -1,6 +1,31 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Proyecto {
 
     public static void main(String[] args) {
-        
+        LinkedList<Empleado> empleados= new LinkedList();
+        Scanner sc= new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            Empleado item = new Empleado(null, null, null, i, null)
+            System.out.println("Ingrese el nombre:");
+            item.setNombre(sc.next());
+            System.out.println("Ingrese el apellido:");
+            item.setApellido(sc.next());
+            System.out.println("Ingrese la direccion:");
+            item.setDireccion(sc.next());
+            System.out.println("Ingrese la edad:");
+            item.setEdad(sc.nextInt());
+            System.out.println("Ingrese el cargo:");
+            item.setDireccion(sc.next());
+            empleados.add(item);
+        }
+        String cadena = "";
+        for (Empleado empleado : empleados) {
+           cadena= cadena + empleado.getNombre() + "\n" + empleado.getApellido() + "\n"  + "\n" + empleado.getCargo()+ "\n"+ empleado.getDireccion() + "\n" +
+           empleado.getEdad();
+        }
+       
+    
     }
 }
